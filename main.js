795c01a4667b6cac01bd754b8db8f05c6b82ef53
Mradul <121034149+672385@users.noreply.github.com>
2023-01-24 10:58:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -18,26 +18,11 @@
         $(window).resize(toggleNavbarMethod);
     });
     
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+
 
 
     // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
+
 
 
     // Testimonials carousel
